41ae2bb7b85e4d6c0445e198151d36551a4d9d5c
emanuelena49 <emanuelelena021099@gmail.com>
2022-07-15 14:38:40 +0200

    extract in file methods for array host floyd warshall

--- include/host_floyd_warshall.hpp
+++ include/host_floyd_warshall.hpp
@@ -3,13 +3,22 @@
 
 #define min(a,b) ((a < b) ? a : b)
 
+int sum_if_not_infinite(int a, int b, int infinity);
+
+// ---------------------------------------------------------------------------
+// Matrix data structure version
+
 void floyd_warshall(int **matrix, int n);
 void floyd_warshall_blocked(int **matrix, int n, int B);
 void execute_round(int **matrix, int n, int t, int row, int col, int B);
 
-int sum_if_not_infinite(int a, int b, int infinity);
+// ---------------------------------------------------------------------------
+// Array data structure version
 
 
-
+void arr_floyd_warshall(int *matrix, int n);
+void arr_floyd_warshall_blocked(int *matrix, int n, int B);
+// int sum_if_not_infinite(int a, int b, int infinity);
+void arr_execute_round(int *matrix, int n, int t, int row, int col, int B);
 
 #endif
