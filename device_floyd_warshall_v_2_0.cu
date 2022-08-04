4ead858f2f2b1eb8bbbe02bf755a87701795b7a6
emanuelena49 <emanuelelena021099@gmail.com>
2022-08-04 11:54:54 +0200

    remove B as parameter of phases

--- device_floyd_warshall_v_2_0.cu
+++ device_floyd_warshall_v_2_0.cu
@@ -17,10 +17,10 @@
 void floyd_warshall_blocked_device_v_2_0(int *matrix, int n, int B);
 
 //rounds code
-__global__ void execute_round_device_v_2_0_phase_1(int *matrix, int n, int t, int B);
-__global__ void execute_round_device_v_2_0_phase_2_row(int *matrix, int n, int t, int B);
-__global__ void execute_round_device_v_2_0_phase_2_col(int *matrix, int n, int t, int B);
-__global__ void execute_round_device_v_2_0_phase_3(int *matrix, int n, int t, int B);
+__global__ void execute_round_device_v_2_0_phase_1(int *matrix, int n, int t);
+__global__ void execute_round_device_v_2_0_phase_2_row(int *matrix, int n, int t);
+__global__ void execute_round_device_v_2_0_phase_2_col(int *matrix, int n, int t);
+__global__ void execute_round_device_v_2_0_phase_3(int *matrix, int n, int t);
 
 int main() {
 
@@ -61,16 +61,16 @@ int main() {
         // try maximum 15 random B 
         for (int tests = 0; tests < 50 && cur_B_idx < 5; tests++) {
 
-            int b = rand() % (n/2);
-            b = (b == 0) ? n : b;       
+            int B = rand() % (n/2);
+            B = (B == 0) ? n : B;       
 
-            bool test_cond = (n%b == 0);
-            for (int i = 0; (i <= cur_B_idx) && test_cond; i++) test_cond = (b != B[i]);
+            bool test_cond = (n%B == 0);
+            for (int i = 0; (i <= cur_B_idx) && test_cond; i++) test_cond = (B != B[i]);
 
             if (test_cond) {
-                printf("n: %d, B: %d\n", n, b);
-                B[++cur_B_idx] = b;
-                do_arr_floyd_warshall_statistical_test(&floyd_warshall_blocked_device_v_2_0, n, b, 500, RANDOM_SEED, true, 4, true);
+                printf("n: %d, B: %d\n", n, B);
+                B[++cur_B_idx] = B;
+                do_arr_floyd_warshall_statistical_test(&floyd_warshall_blocked_device_v_2_0, n, B, 500, RANDOM_SEED, true, 4, true);
             }
                 
         }
@@ -109,7 +109,7 @@ void floyd_warshall_blocked_device_v_2_0(int *matrix, int n, int B) {
         dim3 num_blocks_phase_1(1, 1);
         dim3 threads_per_block_phase_1(B, B);
 
-        execute_round_device_v_2_0_phase_1<<<num_blocks_phase_1, threads_per_block_phase_1, B*B*sizeof(int)>>>(dev_rand_matrix, n, t, B);
+        execute_round_device_v_2_0_phase_1<<<num_blocks_phase_1, threads_per_block_phase_1, B*B*sizeof(int)>>>(dev_rand_matrix, n, t);
         HANDLE_ERROR(cudaDeviceSynchronize());
 
         // phase 2: all blocks that share a row or a column with the self dependent, so
@@ -118,8 +118,8 @@ void floyd_warshall_blocked_device_v_2_0(int *matrix, int n, int B) {
 
         // dim3 num_blocks_phase_2(1, num_rounds-1);  
 
-        execute_round_device_v_2_0_phase_2_row<<<num_rounds-1, threads_per_block_phase_1, 2*B*B*sizeof(int)>>>(dev_rand_matrix, n, t, B);
-        execute_round_device_v_2_0_phase_2_col<<<num_rounds-1, threads_per_block_phase_1, 2*B*B*sizeof(int)>>>(dev_rand_matrix, n, t, B);
+        execute_round_device_v_2_0_phase_2_row<<<num_rounds-1, threads_per_block_phase_1, 2*B*B*sizeof(int)>>>(dev_rand_matrix, n, t);
+        execute_round_device_v_2_0_phase_2_col<<<num_rounds-1, threads_per_block_phase_1, 2*B*B*sizeof(int)>>>(dev_rand_matrix, n, t);
 
 
         HANDLE_ERROR(cudaDeviceSynchronize());
@@ -128,7 +128,7 @@ void floyd_warshall_blocked_device_v_2_0(int *matrix, int n, int B) {
 
         dim3 num_blocks_phase_3(num_rounds-1, num_rounds-1); 
 
-        execute_round_device_v_2_0_phase_3<<<num_blocks_phase_3, threads_per_block_phase_1, 2*B*B*sizeof(int)>>>(dev_rand_matrix, n, t, B);
+        execute_round_device_v_2_0_phase_3<<<num_blocks_phase_3, threads_per_block_phase_1, 2*B*B*sizeof(int)>>>(dev_rand_matrix, n, t);
         HANDLE_ERROR(cudaDeviceSynchronize()); 
     }
 
@@ -139,7 +139,7 @@ void floyd_warshall_blocked_device_v_2_0(int *matrix, int n, int B) {
 }
 
 
-__global__ void execute_round_device_v_2_0_phase_1(int *matrix, int n, int t, int B) {
+__global__ void execute_round_device_v_2_0_phase_1(int *matrix, int n, int t) {
 
     // Launched block and correspondent position in the matrix
 
@@ -154,34 +154,34 @@ __global__ void execute_round_device_v_2_0_phase_1(int *matrix, int n, int t, in
 
     extern __shared__ int block_t_t_shared[];
 
-    int i = threadIdx.x + t * B;  // row abs index
-    int j = threadIdx.y + t * B;  // col abs index
+    int i = threadIdx.x + t * blockDim.x;  // row abs index
+    int j = threadIdx.y + t * blockDim.x;  // col abs index
 
-    block_t_t_shared[threadIdx.x*B + threadIdx.y] = matrix[i*n + j];
+    block_t_t_shared[threadIdx.x*blockDim.x + threadIdx.y] = matrix[i*n + j];
 
     __syncthreads();
 
     // now k is iterating the relative indexind of (t,t) block 
     // in shared memory (instead of the abs position in matrix)
-    for (int k = 0; k < B; k++) {
+    for (int k = 0; k < blockDim.x; k++) {
 
         int using_k_path = sum_if_not_infinite(
-            block_t_t_shared[threadIdx.x*B + k], 
-            block_t_t_shared[k*B + threadIdx.y], 
+            block_t_t_shared[threadIdx.x*blockDim.x + k], 
+            block_t_t_shared[k*blockDim.x + threadIdx.y], 
             INF
         ); 
 
-        if (using_k_path < block_t_t_shared[threadIdx.x*B + threadIdx.y]) {
-            block_t_t_shared[threadIdx.x*B + threadIdx.y] = using_k_path;
+        if (using_k_path < block_t_t_shared[threadIdx.x*blockDim.x + threadIdx.y]) {
+            block_t_t_shared[threadIdx.x*blockDim.x + threadIdx.y] = using_k_path;
         }
         
         __syncthreads();
     }
 
-    matrix[i*n + j] = block_t_t_shared[threadIdx.x*B + threadIdx.y];
+    matrix[i*n + j] = block_t_t_shared[threadIdx.x*blockDim.x + threadIdx.y];
 }
 
-__global__ void execute_round_device_v_2_0_phase_2_row(int *matrix, int n, int t, int B) {
+__global__ void execute_round_device_v_2_0_phase_2_row(int *matrix, int n, int t) {
 
     // Launched blocks and correspondent position in the matrix
     //  -   blockIdx.x says if I am iterating row or cols, 
@@ -201,40 +201,40 @@ __global__ void execute_round_device_v_2_0_phase_2_row(int *matrix, int n, int t
     extern __shared__ int shared_mem[];
     
     int* block_i_j_shared = &shared_mem[0];
-    int* block_t_t_shared = &shared_mem[B*B];
+    int* block_t_t_shared = &shared_mem[(blockDim.x * blockDim.x)];
 
     // it's a row ...
 
     // abs row index 
-    int i = BLOCK_START(t, B) + threadIdx.x;    
+    int i = BLOCK_START(t, blockDim.x) + threadIdx.x;    
     // abs col index   
-    int j = BLOCK_START(blockIdx.x, B) + threadIdx.y + ((blockIdx.x >= t) ? B : 0); 
+    int j = BLOCK_START(blockIdx.x, blockDim.x) + threadIdx.y + ((blockIdx.x >= t) ? blockDim.x : 0); 
 
     // the block where I am working
-    block_i_j_shared[threadIdx.x*B + threadIdx.y] = matrix[i*n + j];
+    block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y] = matrix[i*n + j];
 
     // the self-dependent block already calculated in this round
-    block_t_t_shared[threadIdx.x*B + threadIdx.y] = matrix[
-        ((BLOCK_START(t, B) + threadIdx.x) * n) + (BLOCK_START(t, B) + threadIdx.y)
+    block_t_t_shared[threadIdx.x*blockDim.x + threadIdx.y] = matrix[
+        ((BLOCK_START(t, blockDim.x) + threadIdx.x) * n) + (BLOCK_START(t, blockDim.x) + threadIdx.y)
     ];
 
     __syncthreads();
 
     // now k is iterating the relative indexind of (t,t) block 
     // in shared memory (instead of the abs position in matrix)
-    for (int k = 0; k < B; k++) {
+    for (int k = 0; k < blockDim.x; k++) {
 
         // Because we are doing rows:
         // -    matrix[i,abs_k] is in block_t_t_shared[threadIdx.x,k]
         // -    matrix[abs_k,j] is in block_i_j_shared[k,threadIdx.y]
         int using_k_path = sum_if_not_infinite(
-            block_t_t_shared[threadIdx.x*B + k], 
-            block_i_j_shared[k*B + threadIdx.y], 
+            block_t_t_shared[threadIdx.x*blockDim.x + k], 
+            block_i_j_shared[k*blockDim.x + threadIdx.y], 
             INF
         ); 
 
-        if (using_k_path < block_i_j_shared[threadIdx.x*B + threadIdx.y]) {
-            block_i_j_shared[threadIdx.x*B + threadIdx.y] = using_k_path;
+        if (using_k_path < block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y]) {
+            block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y] = using_k_path;
         }
 
         //printf("i:%d, j:%d, k:%d\n", i, j, k);
@@ -243,10 +243,10 @@ __global__ void execute_round_device_v_2_0_phase_2_row(int *matrix, int n, int t
     }
 
     // copy result in global memory
-    matrix[i*n + j] = block_i_j_shared[threadIdx.x*B + threadIdx.y];
+    matrix[i*n + j] = block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y];
 }
 
-__global__ void execute_round_device_v_2_0_phase_2_col(int *matrix, int n, int t, int B) {
+__global__ void execute_round_device_v_2_0_phase_2_col(int *matrix, int n, int t) {
 
     // Launched blocks and correspondent position in the matrix
     //  -   blockIdx.x says if I am iterating row or cols, 
@@ -266,40 +266,40 @@ __global__ void execute_round_device_v_2_0_phase_2_col(int *matrix, int n, int t
     extern __shared__ int shared_mem[];
 
     int* block_i_j_shared = &shared_mem[0];
-    int* block_t_t_shared = &shared_mem[B*B];
+    int* block_t_t_shared = &shared_mem[blockDim.x*blockDim.x];
 
     // it's a column ...
 
     // abs row index 
-    int i = BLOCK_START(blockIdx.x, B) + threadIdx.x + ((blockIdx.x >= t) ? B : 0);
+    int i = BLOCK_START(blockIdx.x, blockDim.x) + threadIdx.x + ((blockIdx.x >= t) ? blockDim.x : 0);
     // abs col index 
-    int j = BLOCK_START(t, B) + threadIdx.y;
+    int j = BLOCK_START(t, blockDim.x) + threadIdx.y;
 
     // the block where I am working
-    block_i_j_shared[threadIdx.x*B + threadIdx.y] = matrix[i*n + j];
+    block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y] = matrix[i*n + j];
 
     // the self-dependent block already calculated in this round
-    block_t_t_shared[threadIdx.x*B + threadIdx.y] = matrix[
-        ((BLOCK_START(t, B) + threadIdx.x) * n) + (BLOCK_START(t, B) + threadIdx.y)
+    block_t_t_shared[threadIdx.x*blockDim.x + threadIdx.y] = matrix[
+        ((BLOCK_START(t, blockDim.x) + threadIdx.x) * n) + (BLOCK_START(t, blockDim.x) + threadIdx.y)
     ];
     
     __syncthreads();
 
     // now k is iterating the relative indexind of (t,t) block 
     // in shared memory (instead of the abs position in matrix)
-    for (int k = 0; k < B; k++) {
+    for (int k = 0; k < blockDim.x; k++) {
         
         // Because we are doing columns:
         // -    matrix[i,k] is in block_i_j_shared[threadIdx.x,k]
         // -    matrix[k,j] is in block_t_t_shared[k,threadIdx.y]
         int using_k_path = sum_if_not_infinite(
-            block_i_j_shared[threadIdx.x*B + k], 
-            block_t_t_shared[k*B + threadIdx.y], 
+            block_i_j_shared[threadIdx.x*blockDim.x + k], 
+            block_t_t_shared[k*blockDim.x + threadIdx.y], 
             INF
         ); 
 
-        if (using_k_path < block_i_j_shared[threadIdx.x*B + threadIdx.y]) {
-            block_i_j_shared[threadIdx.x*B + threadIdx.y] = using_k_path;
+        if (using_k_path < block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y]) {
+            block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y] = using_k_path;
         }
 
         //printf("i:%d, j:%d, k:%d\n", i, j, k);
@@ -308,11 +308,11 @@ __global__ void execute_round_device_v_2_0_phase_2_col(int *matrix, int n, int t
     }
 
     // copy result in global memory
-    matrix[i*n + j] = block_i_j_shared[threadIdx.x*B + threadIdx.y];
+    matrix[i*n + j] = block_i_j_shared[threadIdx.x*blockDim.x + threadIdx.y];
 }
 
 
-__global__ void execute_round_device_v_2_0_phase_3(int *matrix, int n, int t, int B) {
+__global__ void execute_round_device_v_2_0_phase_3(int *matrix, int n, int t) {
 
     // Launched blocks and correspondent position in the matrix
 
@@ -332,12 +332,12 @@ __global__ void execute_round_device_v_2_0_phase_3(int *matrix, int n, int t, in
     extern __shared__ int shared_mem[];
 
     int* block_i_t_shared = &shared_mem[0];
-    int* block_t_j_shared = &shared_mem[B*B];
+    int* block_t_j_shared = &shared_mem[blockDim.x*blockDim.x];
 
     // abs row index
-    int i = threadIdx.x + blockIdx.x * blockDim.x + ((blockIdx.x >= t) ? B : 0);
+    int i = threadIdx.x + blockIdx.x * blockDim.x + ((blockIdx.x >= t) ? blockDim.x : 0);
     // abs col index
-    int j = threadIdx.y + blockIdx.y * blockDim.y + ((blockIdx.y >= t) ? B : 0);
+    int j = threadIdx.y + blockIdx.y * blockDim.y + ((blockIdx.y >= t) ? blockDim.x : 0);
     
     // since the cell i,j is read and written only by this thread
     // there is no need to copy its value to shared memory we can just us a local variable
@@ -345,22 +345,22 @@ __global__ void execute_round_device_v_2_0_phase_3(int *matrix, int n, int t, in
         
     // In phase 3 I copy in two portions of my shared memory
     // the block corresponding to (t, this column) and (this row, t)
-    block_i_t_shared[threadIdx.x*B + threadIdx.y] = matrix[
-        i*n + (BLOCK_START(t, B) + threadIdx.y)
+    block_i_t_shared[threadIdx.x*blockDim.x + threadIdx.y] = matrix[
+        i*n + (BLOCK_START(t, blockDim.x) + threadIdx.y)
     ];
-    block_t_j_shared[threadIdx.x*B + threadIdx.y] = matrix[
-        ((BLOCK_START(t, B) + threadIdx.x) * n) + j
+    block_t_j_shared[threadIdx.x*blockDim.x + threadIdx.y] = matrix[
+        ((BLOCK_START(t, blockDim.x) + threadIdx.x) * n) + j
     ];
     
     __syncthreads();
 
     // now k is iterating the relative indexind of (t,t) block 
     // in shared memory (instead of the abs position in matrix)
-    for (int k = 0; k < B; k++) {
+    for (int k = 0; k < blockDim.x; k++) {
 
         int using_k_path = sum_if_not_infinite(
-            block_i_t_shared[threadIdx.x*B + k],
-            block_t_j_shared[k*B + threadIdx.y],
+            block_i_t_shared[threadIdx.x*blockDim.x + k],
+            block_t_j_shared[k*blockDim.x + threadIdx.y],
             INF
         ); 
 
